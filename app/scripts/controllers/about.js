73ea0f15115e6dfe72df90fa6dc6dc762526e8d7
Karen Duenas <karen@Karens-MacBook-Air.local>
2016-08-20 13:12:07 -0700

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xpertwandererApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xpertwandererApp
+ */
+angular.module('xpertwandererApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xpertwandererApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xpertwandererApp
+ */
+angular.module('xpertwandererApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
